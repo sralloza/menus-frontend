d8e321bffbdb46de36cbc868a913437aa98b6d75
sralloza <sralloza@gmail.com>
2020-10-18 14:25:25 +0200

    make web installable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenusComponent } from './menus/menus.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
   declarations: [AppComponent, MenusComponent],
-  imports: [AppRoutingModule, BrowserModule, CommonModule, NgbModule],
+  imports: [AppRoutingModule, BrowserModule, CommonModule, NgbModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
